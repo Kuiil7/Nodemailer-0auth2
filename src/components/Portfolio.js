7657d98160c7474b971dea245f0fe6bda859cdd0
darh0ky <jgamerino@gamil.com>
2020-07-13 10:45:30 -0700

    added 2 portfolio links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,8 +2,30 @@ import React from "react";
 
 function Portfolio () {
   return (
-<div className="sectionsPor bg-dark">
-    Port Parking
+    <div>
+      <center>
+<div className="card mb-3">
+<img src="https://i.imgrz.com/6orP.png" class="card-img-top" alt="..." />
+
+  <div className="card-body">
+    <h5 className="card-title">Sports Tracker</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+
+</div>
+
+<div className="card">
+<img src="https://i.imgrz.com/6uUZ.png" class="card-img-top" alt="..." />
+
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+
+</div>
+</center>
 </div>
   );
 }
