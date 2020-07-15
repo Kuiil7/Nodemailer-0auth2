a9bfc62a4e818a897848f06a49f33fcaf3b95431
darh0ky <jgamerino@gamil.com>
2020-07-14 22:44:55 -0700

    heroku d

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mailer from './mailer'
 
+
+const path = require('path')
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('*', (req, res) => {
   res.send('Server is working. Please post at "/contact" to submit a message.')
